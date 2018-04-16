0509c658de37c8fe671f79e9635261804c083da7
mr.j <915171374@qq.com>
2018-04-16 18:45:27 +0800

    textview设置点击事件

--- MyFirstTestDemo/app/src/main/java/com/example/mrj/myfirsttestdemo/MainActivity.java
+++ MyFirstTestDemo/app/src/main/java/com/example/mrj/myfirsttestdemo/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.mrj.myfirsttestdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initID();
+    }
+
+    private void initID() {
+        textView = findViewById(R.id.tv);
+        textView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
