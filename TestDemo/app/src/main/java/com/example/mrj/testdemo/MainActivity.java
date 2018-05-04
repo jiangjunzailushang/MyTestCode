c59dd6202bc8b25aba536714477bb7ba3b2eea20
jiangrenzhong <915171374@qq.com>
2018-05-04 20:12:23 +0800

    我增加了一行注释，按理说在使用git pull之后会提示冲突的  但是现在没有提示

--- TestDemo/app/src/main/java/com/example/mrj/testdemo/MainActivity.java
+++ TestDemo/app/src/main/java/com/example/mrj/testdemo/MainActivity.java
@@ -8,8 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        
+        //第二次的修改
         setContentView(R.layout.activity_main);
     }
 }
