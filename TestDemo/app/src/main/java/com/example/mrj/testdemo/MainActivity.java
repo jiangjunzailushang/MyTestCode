0b2cfb88ce104e6e2fc28b4798fdf6940aaae426
Aaron <15093322046@163.com>
2018-05-04 20:45:54 +0800

    新增textView1

--- TestDemo/app/src/main/java/com/example/mrj/testdemo/MainActivity.java
+++ TestDemo/app/src/main/java/com/example/mrj/testdemo/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //第三次的修改
         TestView TestView = new TextView(this);
+        TestView TestView1 = new TextView(this);
     }
 }
